25cdbfd8959375dc1260e93d0db8244c1cb808ff
David Gomes <LegendL3n@users.noreply.github.com>
2022-01-29 09:09:22 +0000

    Fixed bot update not updating and starting at midnight, if it wasn't already running

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kill $(pwdx $(pgrep -f "java.*quarkus") 2>/dev/null | grep Pensador | cut -d ":" -f 1)
+kill "$(pwdx "$(pgrep -f "java.*quarkus")" 2>/dev/null | grep Pensador | cut -d ":" -f 1)" 2>/dev/null || exit 0
